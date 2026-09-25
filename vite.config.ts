d2f16ec8a6a71410daeb47a0e40f10e903364ed4
forhadalpha08-ui <forhadalpha08@gmail.com>
2026-09-25 23:23:33 +0400

    chore: update vite watcher ignore rules

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
     watch: {
-      ignored: ['**/agent/**', '**/node_modules/**'],
+      ignored: ['**/agent/**', '**/node_modules/**', '**/*.zip', '**/.git/**'],
     },
   },
 });
